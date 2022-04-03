3b06f3f7e1c92cea3d1e6e70bd0ccf8acee0e150
Amit Ofeck <Amitofeck1@gmail.com>
2022-04-03 15:50:47 +0300

    create server

--- client/src/Pages/Teams.js
+++ client/src/Pages/Teams.js
@@ -1,12 +1,16 @@
-import React from 'react';
-const axios = require("axios");
+import React, { useEffect, useState } from 'react';
+import Utils from '../Utils/Utils';
 
 
 function Teams(props) {
 
+    // const [teams , setTeams] = useState([])
 
-
-
+    useEffect(async () => {
+        let answer = await Utils.getTeams()
+        console.log(answer)
+        // setTeams(answer)    
+    },[])
 
     return (
         <div>
---
+++ client/src/Utils/Utils.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+// const localUrl = "http://localhost:8000/games/" 
+// const productionUrl = "https://draw-guess-app-server.herokuapp.com/games/"
+// const baseUrl = localUrl // if we want back to local , use localUrl
+
+const fantasyPremierLeagueAPI = "https://fantasy.premierleague.com/api/bootstrap-static/";
+const testAPI = "https://api.tvmaze.com/shows";
+const obj = {
+    method: 'GET',
+    url: 'https://fantasy.premierleague.com/api/bootstrap-static/',
+    // url: 'https://footballapi.pulselive.com/football/',
+    // params: {season: '2020', league: '39'},
+    headers: {
+    "Origin": 'https://www.premierleague.com/'
+    }
+}
+
+
+const getTeams = async () => {
+    console.log("func")
+    const res = await axios.get('https://fantasy.premierleague.com/api/bootstrap-static/', {
+    headers: {
+    'Origin': 'https://www.premierleague.com/' ,
+    mode: 'cors',
+    credentials: 'include'
+    }
+    });
+    return res.data
+
+    // console.log("func")
+    // let resp = await axios.request(fantasyPremierLeagueAPI);
+    // console.log(resp)
+    // return resp.data
+
+}
+
+
+export default {getTeams}
