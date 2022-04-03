2f16f8c54951380f70cd49e6c3dc774a7b02e673
Amit Ofeck <Amitofeck1@gmail.com>
2022-04-03 16:25:19 +0300

    get all teams function

--- server/models/dataBL.js
+++ server/models/dataBL.js
@@ -1,12 +1,21 @@
 // let gameSchema = require('./gameSchema')
 
-// const getTeams =  async (GameId) => {
-    
-//     return await gameSchema.find({GameId: GameId})
-// }
+const axios = require('axios')
 
-const getTeams = () => {
+const premierleagueAPI = {
+    method: 'GET',
+    url: 'https://fantasy.premierleague.com/api/bootstrap-static/',
+    // url: 'https://footballapi.pulselive.com/football/',
+    // params: {season: '2020', league: '39'},
+    headers: {
+    "Origin": 'https://www.premierleague.com/'
+    }
+}
+
+const getTeams = async () => {
       console.log("get teams")
+      let resp = await axios.request(premierleagueAPI);
+      return resp.data.teams
     }
 
 
