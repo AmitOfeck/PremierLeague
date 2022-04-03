f1c8a279734820ab68ecb9470a20013451f4bec1
Amit Ofeck <Amitofeck1@gmail.com>
2022-04-03 12:40:33 +0300

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Teams from './Pages/Teams';
+
+function App() {
+  return (
+    <div className="App">
+     hi
+     <Teams/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Pages/Teams.js
@@ -0,0 +1,18 @@
+import React from 'react';
+const axios = require("axios");
+
+
+function Teams(props) {
+
+
+
+
+
+    return (
+        <div>
+            teams
+        </div>
+    );
+}
+
+export default Teams;
