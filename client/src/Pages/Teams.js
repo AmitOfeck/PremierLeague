6dc1dde184021397fea4e3f28889980c023bc98a
Amit Ofeck <Amitofeck1@gmail.com>
2022-04-03 16:53:00 +0300

    premier league table

--- client/src/Pages/Teams.js
+++ client/src/Pages/Teams.js
@@ -12,9 +12,40 @@ function Teams(props) {
         setTeams(answer)    
     },[])
 
+    let body = teams.map((team, index) => {
+        return (<tr key={team.id}>
+        <th scope="row">{team.id}</th>
+        <td>{team.name}</td>
+        <td>{team.points}</td>
+        <td>{team.played}</td>
+        <td>{team.win}</td>
+        <td>{team.draw}</td>
+        <td>{team.loss}</td>
+        </tr>)
+    })
+
     return (
         <div>
             teams
+
+            <table class="table">
+              <thead>
+                 <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Points</th>
+                    <th scope="col">Played</th>
+                    <th scope="col">Win</th>
+                    <th scope="col">Draw</th>
+                    <th scope="col">Loss</th>
+                 </tr>
+            </thead>
+            <tbody>
+             {body}
+           </tbody>
+           </table>
+
+            
         </div>
     );
 }
