ddc04b0a7d349dbff370a83971bed370a455a5d4
Amit Ofeck <Amitofeck1@gmail.com>
2022-04-05 16:41:08 +0300

    show stats

--- client/src/Pages/FixtureResult.js
+++ client/src/Pages/FixtureResult.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../App.css';
 
 function FixtureResult(props) {
 
+    let [showStats , setShowStats] = useState(false)
+
+
     const homeTeam = getTeamNameById(props.data.team_h);
     const awayTeam = getTeamNameById(props.data.team_a);
-    const team_h_score = props.data.team_h_score;
-    const team_a_score = props.data.team_a_score;
-
+    const team_h_score = props.data.started? props.data.team_h_score : "";
+    const team_a_score = props.data.started? props.data.team_a_score : "";
+    const time = !props.data.started ? timeConvert(props.data.kickoff_time) : "";
 
     function getTeamNameById(id){
         return props.teamsInfo[id-1].name
     }
 
+    function timeConvert(date){
+        let convert = new Date(date)
+        console.log(convert)
+        return convert.toTimeString().slice(0,5)
+    }
+
+    let visibility = showStats ? "" : "hiddenDiv";
+    
 
 
     return (
         <div>
-            <p>{homeTeam}</p>{team_h_score} : {team_a_score}<p>{awayTeam}</p>
+            <div id="gameResult" onClick={() => setShowStats(!showStats)}>
+            <p id="teamName">{homeTeam} &nbsp; {team_h_score} - {team_a_score}  &nbsp; {awayTeam}</p>
+            {time}
+            </div>
+
+            <div class="gameStats" id={visibility}>
+            hi
+            </div>
+
+            <br/><br/>
         </div>
     );
 }
