83d6c76e8fbf925f53463ccbb10117deaec5ecae
Amit Ofeck <Amitofeck1@gmail.com>
2022-04-06 18:20:10 +0300

    css stats

--- client/src/Pages/StatComp.js
+++ client/src/Pages/StatComp.js
@@ -12,21 +12,23 @@ function stats(props) {
 
     let awayPlayers = props.data.a.length > 0 ?props.data.a.map((player, index) => {
         let name = getPlayersNameById(player.element)
-        return (<p key={index}>{name}</p>) 
+        return (<p key={index}>{name} ({player.value})</p>) 
     }) : <div></div>
 
     let homePlayers = props.data.h.length > 0 ?props.data.h.map((player, index) => {
         let name = getPlayersNameById(player.element)
-        return (<p key={index}>{name}</p>) 
+        return (<p key={index}>{name} ({player.value})</p>) 
     }) : <div></div>
 
 
 
     return (
         <div>
-            {props.data.identifier}
+            <div id="statTitle">{props.data.identifier}</div>
+            <div id="homeAway">
             <div>{homePlayers}</div>
             <div>{awayPlayers}</div>
+            </div>
         </div>
     );
 }
