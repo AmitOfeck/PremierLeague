a006bb469ae075eda99f831a2a910817215392bc
Amit Ofeck <Amitofeck1@gmail.com>
2022-04-14 13:49:38 +0300

    popo up details

--- client/src/Pages/Popup.js
+++ client/src/Pages/Popup.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Utils from './Utils/Utils';
 import '../App.css';
+import ball from '../images/ball.jpeg';
+import shoe from '../images/show.jpeg';
+import yellow from '../images/yellow.jpeg';
+import red from '../images/red.jpeg';
+import clock from '../images/clock.jpeg';
 
 
 function Popup(props) {
@@ -29,6 +34,38 @@ function Popup(props) {
         <div className="popup-box">
           <div className="box">
             <span className="close-icon" onClick={() => setIsOpen(!isOpen)}>x</span>
+
+            <div>
+                <p>{props.playerData.first_name}{" "}{props.playerData.second_name}</p>
+                <table class="table table-hover">
+                <thead id="freeze">
+                   <tr>
+                      <th scope="col"><img id="imagePlayersData" src={ball}/></th>
+                      <th scope="col"><img id="imagePlayersData" src={shoe}/></th>
+                      <th scope="col"><img id="imagePlayersData" src={yellow}/></th>
+                      <th scope="col"><img id="imagePlayersData" src={red}/></th>
+                      <th scope="col"><img id="imagePlayersData" src={clock}/></th>
+                   </tr>
+                </thead>
+                <tbody>
+                      <td>{props.playerData.goals_scored}</td>
+                      <td>{props.playerData.assists}</td>
+                      <td>{props.playerData.yellow_cards}</td>
+                      <td>{props.playerData.red_cards}</td>
+                      <td>{props.playerData.minutes}</td>
+                </tbody>
+                </table>
+            </div>
+
+            <ul class="nav nav-tabs">
+               <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" >Active</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+
             <p>{props.playerId}</p>
           </div>
         </div>
